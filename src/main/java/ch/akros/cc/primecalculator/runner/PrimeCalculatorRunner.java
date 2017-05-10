219d943938419de9d24e655d2deb1e0ad2de6c0f
Flagman <patrick@flagmansworld.ch>
2017-05-10 22:26:28 +0200

    Reformat

--- src/main/java/ch/akros/cc/primecalculator/runner/PrimeCalculatorRunner.java
+++ src/main/java/ch/akros/cc/primecalculator/runner/PrimeCalculatorRunner.java
@@ -3,12 +3,7 @@
  */
 package ch.akros.cc.primecalculator.runner;
 
-import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
-
+import ch.akros.cc.primecalculator.worker.PrimeCalculatorWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +11,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-
-import ch.akros.cc.primecalculator.worker.PrimeCalculatorWorker;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.util.FormatUtil;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Runner Thread to execute a calculation thread each 1000 numbers.
@@ -31,72 +30,72 @@ import oshi.util.FormatUtil;
 @Scope("prototype")
 public class PrimeCalculatorRunner implements Runnable {
 
-	private static final Logger LOG = LoggerFactory.getLogger(PrimeCalculatorRunner.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PrimeCalculatorRunner.class);
 
-	private final Map<Long, Long> numbersToCalculatePrime = new HashMap<>();
+    private final Map<Long, Long> numbersToCalculatePrime = new HashMap<>();
 
-	@Autowired
-	private ThreadPoolTaskExecutor taskExecutor;
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
 
-	@Autowired
-	private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Runnable#run()
-	 */
-	@Override
-	public void run() {
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run() {
 
-		final SystemInfo si = new SystemInfo();
-		final HardwareAbstractionLayer hal = si.getHardware();
+        final SystemInfo si = new SystemInfo();
+        final HardwareAbstractionLayer hal = si.getHardware();
 
-		for (final Entry<Long, Long> numberToCalculatePrime : numbersToCalculatePrime.entrySet()) {
-			final PrimeCalculatorWorker worker = applicationContext.getBean(PrimeCalculatorWorker.class);
-			worker.setNumberRange(numberToCalculatePrime.getKey(), numberToCalculatePrime.getValue());
-			taskExecutor.execute(worker);
-		}
+        for (final Entry<Long, Long> numberToCalculatePrime : numbersToCalculatePrime.entrySet()) {
+            final PrimeCalculatorWorker worker = applicationContext.getBean(PrimeCalculatorWorker.class);
+            worker.setNumberRange(numberToCalculatePrime.getKey(), numberToCalculatePrime.getValue());
+            taskExecutor.execute(worker);
+        }
 
-		while (taskExecutor.getActiveCount() != 0) {
-			printMemory(hal.getMemory());
-			printCpu(hal.getProcessor());
-			try {
-				TimeUnit.SECONDS.sleep(5);
-			} catch (final InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-			if (Thread.currentThread().isInterrupted()) {
-				break;
-			}
-		}
-		taskExecutor.shutdown();
-	}
+        while (taskExecutor.getActiveCount() != 0) {
+            printMemory(hal.getMemory());
+            printCpu(hal.getProcessor());
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (final InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            if (Thread.currentThread().isInterrupted()) {
+                break;
+            }
+        }
+        taskExecutor.shutdown();
+    }
 
-	private void printMemory(final GlobalMemory memory) {
+    private void printMemory(final GlobalMemory memory) {
 
-		final long usedMemory = memory.getTotal() - memory.getAvailable();
-		LOG.debug("UsedMemory={}/{}", FormatUtil.formatBytes(usedMemory).replaceAll(" ", ""),
-				FormatUtil.formatBytes(memory.getTotal()).replaceAll(" ", ""));
-	}
+        final long usedMemory = memory.getTotal() - memory.getAvailable();
+        LOG.debug("UsedMemory={}/{}", FormatUtil.formatBytes(usedMemory).replaceAll(" ", ""),
+                FormatUtil.formatBytes(memory.getTotal()).replaceAll(" ", ""));
+    }
 
-	private void printCpu(final CentralProcessor processor) {
+    private void printCpu(final CentralProcessor processor) {
 
-		final DecimalFormat df = new DecimalFormat("#.00");
-		LOG.debug("CpuLoad={}", df.format(processor.getSystemCpuLoad() * 100));
-	}
+        final DecimalFormat df = new DecimalFormat("#.00");
+        LOG.debug("CpuLoad={}", df.format(processor.getSystemCpuLoad() * 100));
+    }
 
-	public void setNumberRange(final long begin, final long end) {
+    public void setNumberRange(final long begin, final long end) {
 
-		long indexBegin = begin;
-		do {
-			long indexEnd = indexBegin + 1000;
-			if (indexEnd > end) {
-				indexEnd = end;
-			}
-			numbersToCalculatePrime.put(indexBegin, indexEnd);
-			indexBegin += 1000;
-		} while (indexBegin <= end);
-	}
+        long indexBegin = begin;
+        do {
+            long indexEnd = indexBegin + 1000;
+            if (indexEnd > end) {
+                indexEnd = end;
+            }
+            numbersToCalculatePrime.put(indexBegin, indexEnd);
+            indexBegin += 1000;
+        } while (indexBegin <= end);
+    }
 
 }
